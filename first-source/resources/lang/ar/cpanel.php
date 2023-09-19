ed9eb47a31e350008ff3853c4bb65712f4cd837a
Abdulsalam Sawalha <109074523+AbdulsalamSaw@users.noreply.github.com>
2023-09-19 15:06:49 +0300

    add language

--- first-source/resources/lang/ar/cpanel.php
+++ first-source/resources/lang/ar/cpanel.php
@@ -3,8 +3,17 @@
 return [
     'cpanel' => 'لوحة التحكم',
     'logout' => 'تسجيل الخروج',
-    'logged' => 'تم تسجيل الدخول كـ',
-    'dashboard' => 'لوحة القيادة', 
-    'settings' => 'الإعدادات', 
+    'logged' => 'تم تسجيل الدخول باسم',
+    'dashboard' => 'لوحة القيادة',
+    'welcome' => 'مرحبًا',
+    'settings' => 'الإعدادات',
+    'articles' => 'المقالات',
+    'user' => 'المستخدم',
+    'visitors' => 'الزوار',
+    'categories' => 'الفئات',
+    'chart_user' => 'مخطط المستخدم',
+    'chart_visitor' => 'مخطط الزائر',
+    'chart_category' => 'مخطط الفئة',
+    'chart_article' => 'مخطط المقالة',
 ];
 
--- first-source/resources/views/admin/home/index.blade.php
+++ first-source/resources/views/admin/home/index.blade.php
@@ -1,11 +1,12 @@
 @extends('admin.app.layout')
-@section('title', 'Home Page')
+@section('title', __('cpanel.home_page'))
+
 @section('content')
 <div class="row">
     <div class="col-lg-3 col-md-6">
         <div class="card bg-primary text-white mb-4">
             <div class="card-body">
-                <h4>User</h4>
+                <h4>{{ __('cpanel.user') }}</h4>
             </div>
             <div class="card-footer d-flex align-items-center justify-content-between">
                 <div class="PatientNumber">
@@ -17,7 +18,7 @@
     <div class="col-lg-3 col-md-6">
         <div class="card bg-danger text-white mb-4">
             <div class="card-body">
-                <h4>Visitors</h4>
+                <h4>{{ __('cpanel.visitors') }}</h4>
             </div>
             <div class="card-footer d-flex align-items-center justify-content-between">
                 <div class="TestNumber">
@@ -29,7 +30,7 @@
     <div class="col-lg-3 col-md-6">
         <div class="card bg-success text-white mb-4">
             <div class="card-body">
-                <h4>Categories</h4>
+                <h4>{{ __('cpanel.categories') }}</h4>
             </div>
             <div class="card-footer d-flex align-items-center justify-content-between">
                 <div class="TestNumber">
@@ -42,7 +43,7 @@
     <div class="col-lg-3 col-md-6">
         <div class="card bg-danger text-white mb-4">
             <div class="card-body">
-                <h4>Articles</h4>
+                <h4>{{ __('cpanel.articles') }}</h4>
             </div>
             <div class="card-footer d-flex align-items-center justify-content-between">
                 <div class="TestNumber">
@@ -53,60 +54,45 @@
     </div>
 </div>
 <div class="row">
-    <div class = "col-lg-6 col-md-6">
+    <div class="col-lg-6 col-md-6">
         <div class="card-body">
-            <h4>Chart User</h4>
-            {!!$chart_user->renderHtml()!!}
-
+            <h4>{{ __('cpanel.chart_user') }}</h4>
+            {!! $chart_user->renderHtml() !!}
         </div>
     </div>
-    <div class = "col-lg-6 col-md-6">
+    <div class="col-lg-6 col-md-6">
         <div class="card-body">
-            <h4>Chart Visitor</h4>
-            {!!$chart_visitor->renderHtml()!!}
+            <h4>{{ __('cpanel.chart_visitor') }}</h4>
+            {!! $chart_visitor->renderHtml() !!}
         </div>
     </div>
 </div>
 
 <div class="row">
-    <div class = "col-lg-6 col-md-6">
+    <div class="col-lg-6 col-md-6">
         <div class="card-body">
-            <h4>Chart Category</h4>
-            {!!$chart_category->renderHtml()!!}
-
+            <h4>{{ __('cpanel.chart_category') }}</h4>
+            {!! $chart_category->renderHtml() !!}
         </div>
     </div>
-    <div class = "col-lg-6 col-md-6">
+    <div class="col-lg-6 col-md-6">
         <div class="card-body">
-            <h4>Chart Article</h4>
-            {!!$chart_article->renderHtml()!!}
+            <h4>{{ __('cpanel.chart_article') }}</h4>
+            {!! $chart_article->renderHtml() !!}
         </div>
     </div>
 </div>
 
+{!! $chart_user->renderChartJsLibrary() !!}
+{!! $chart_user->renderJs() !!}
 
-{!!$chart_user->renderChartJsLibrary()!!}
+{!! $chart_visitor->renderChartJsLibrary() !!}
+{!! $chart_visitor->renderJs() !!}
 
-{!!$chart_user->renderJs()!!}
+{!! $chart_category->renderChartJsLibrary() !!}
+{!! $chart_category->renderJs() !!}
 
-{!!$chart_visitor->renderChartJsLibrary()!!}
-
-{!!$chart_visitor->renderJs()!!}
-
-
-{!!$chart_category->renderChartJsLibrary()!!}
-
-{!!$chart_category->renderJs()!!}
-
-
-
-{!!$chart_article->renderChartJsLibrary()!!}
-
-{!!$chart_article->renderJs()!!}
-
-
-
-
-
+{!! $chart_article->renderChartJsLibrary() !!}
+{!! $chart_article->renderJs() !!}
 
 @endsection
